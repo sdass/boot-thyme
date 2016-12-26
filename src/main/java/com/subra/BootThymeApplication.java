776e013315d85ccf6db67989927e257a6c95c968
sdass <sdass@drf.com>
2016-12-26 08:30:07 -0500

    initial commit from sprint-io

---
+++ src/main/java/com/subra/BootThymeApplication.java
@@ -0,0 +1,12 @@
+package com.subra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootThymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootThymeApplication.class, args);
+	}
+}
